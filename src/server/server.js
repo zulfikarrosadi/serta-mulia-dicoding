825b0fcd4bc2a1654f3d63b41b8b9764eb080b24
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-12-03 05:54:11 +0700

    fix(server): update server host

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ const loadModel = require('../services/loadModel');
 const init = async () => {
 	const server = Hapi.server({
 		port: 3000,
-		host: 'localhost',
+		host: process.env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0',
 		routes: {
 			cors: {
 				origin: ['*']
